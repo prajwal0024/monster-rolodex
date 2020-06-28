2ffd4e742207d84e805d8aa69d7a171620f3dbca
prajwal0024 <prajwal0024@gmail.com>
2020-06-28 20:51:00 +0530

    Updates

---
+++ precache-manifest.1a6179652227198b71366190d624a23c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a522e2b957d245cc482a8e7e10aec8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "af3bf865ccd5cd543ba5",
+    "url": "/monster-rolodex/static/css/main.13ff8cc3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3bf865ccd5cd543ba5",
+    "url": "/monster-rolodex/static/js/main.088a0502.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
